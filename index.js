2654f25c343ea2b40af1d7a5a78626571f173a48
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-03-20 16:50:14 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'John';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+const longGreeting = `Welcome, ${currentUser}!`;
+
+const greeting = `Welcome, ${currentUser}!`;
